e2ef5dfa8ff985803ce833373d6cd3791ac739a6
Peter-Simon Dieterich <dieterich.peter@gmail.com>
2019-11-04 20:07:13 +0100

    Fixed typo in system-sleep script

--- system/ir-led.sh.cmake
+++ system/ir-led.sh.cmake
@@ -3,6 +3,6 @@
 # Enable IR emitter after waking from suspend/hibernation
 case $1 in
     post)
-	$CMAKE_INSTALL_PREFIX@/bin/chicony-ir-toggle on
+	@CMAKE_INSTALL_PREFIX@/bin/chicony-ir-toggle on
 	;;
 esac
